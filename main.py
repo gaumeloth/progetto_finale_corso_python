f749d9c533bfae3623fd1fd3764fd28944945a24
gaumeloth <guido.discacciati@gmail.com>
2025-10-02 09:59:14 +0200

    beta

--- main.py
+++ main.py
@@ -1,6 +1,18 @@
-def main():
-    print("Hello from progetto-finale!")
+#!/usr/bin/env python3
+from ui import MainUI
+from kivy.app import App
+from kivy.lang import Builder
+
+# carichiamo il file kv esterno (nome: nmap.kv)
+Builder.load_file("nmap.kv")
+
+# Importiamo MainUI dalla versione modularizzata
 
 
-if __name__ == "__main__":
-    main()
+class NmapKivyApp(App):
+    def build(self):
+        return MainUI()
+
+
+if __name__ == '__main__':
+    NmapKivyApp().run()
